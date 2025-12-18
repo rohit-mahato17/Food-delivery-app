dad3ef6447e33ecd7bd48058f0ac60ba273a473a
Rohit Mahato <rohitmahato88935@gmail.com>
2025-12-18 22:29:39 +0530

    UI

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,11 @@ import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home/Home'
 import Cart from './pages/Cart/Cart'
 import PlaceOrder from './pages/PlaceOrder/PlaceOrder'
+import Footer from './components/Footer/Footer'
 
 const App = () => {
   return (
+    <>
     <div className='app'>
       <Navbar />
 
@@ -16,6 +18,8 @@ const App = () => {
         <Route path='/order' element={<PlaceOrder />} />
       </Routes>
     </div>
+    <Footer/>
+    </>
   )
 }
 
