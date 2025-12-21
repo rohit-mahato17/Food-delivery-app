f299f605fd76e149c471c40746bccff7b7486b79
Rohit Mahato <rohitmahato88935@gmail.com>
2025-12-21 12:00:02 +0530

    update the code

--- src/components/FoodItem/FoodItem.jsx
+++ src/components/FoodItem/FoodItem.jsx
@@ -40,7 +40,7 @@ const FoodItem = ({ id, name, price, description, image }) => {
            <div className="food-item-info">
               <div className="food-item-name-rating">
                 <p>{name}</p>
-                <img src={assets.rating_stars} alt="" />
+                <img src={assets.rating_starts} alt="" />
               </div>
 
               <p className="food-item-desc">{description}</p>
